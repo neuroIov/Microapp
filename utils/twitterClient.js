2d84e6b388809a2e63054a63e1ddb6bd961585a5
binarybodhi <neohex262@gmail.com>
2024-08-28 14:46:16 +0400

    Create twitterClient.js

---
+++ utils/twitterClient.js
@@ -0,0 +1,10 @@
+const { TwitterApi } = require('twitter-api-v2');
+
+const client = new TwitterApi({
+  appKey: process.env.TWITTER_API_KEY,
+  appSecret: process.env.TWITTER_API_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_SECRET,
+});
+
+module.exports = client;
